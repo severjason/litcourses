0258d055810a930cd40be215718ccb0e62883d94
severjason <ssever2010@gmail.com>
2018-03-14 23:52:53 +0200

    App component adjustment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule, Title} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {SearchComponent} from './search/search.component';
+import {ApiService} from './services/api.service';
+import {HttpModule} from '@angular/http';
+import {ReactiveFormsModule} from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'search',
+    pathMatch: 'full',
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'search',
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [
+    Title,
+    ApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
